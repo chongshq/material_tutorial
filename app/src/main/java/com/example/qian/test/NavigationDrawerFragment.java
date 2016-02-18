4d161d2794da0cf99896e4f13d404d246297c384
qian <qian.cs@outlook.com>
2016-02-18 19:20:00 +0800

    no message

---
+++ app/src/main/java/com/example/qian/test/NavigationDrawerFragment.java
@@ -0,0 +1,64 @@
+package com.example.qian.test;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NavigationDrawerFragment extends Fragment {
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private RecyclerView recyclerView;
+
+    public NavigationDrawerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View layout= inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        recyclerView= (RecyclerView) layout.findViewById(R.id.drawerList);
+        return layout;
+    }
+
+    public void setUp(DrawerLayout drawerLayout, final Toolbar toolbar) {
+        mDrawerLayout=drawerLayout;
+        mDrawerToggle=new ActionBarDrawerToggle(getActivity(),drawerLayout,toolbar,R.string.open,R.string.close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();
+            }
+        });
+    }
+}
